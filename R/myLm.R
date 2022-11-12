749f5aa8cf1e3592e610ef551eceb55c95168eab
nanaAsuka <menghuis@uci.edu>
2022-11-11 22:08:01 -0500

    delete square.R and updated it to myLm.R

---
+++ R/myLm.R
@@ -0,0 +1,22 @@
+#'myLm
+#'
+#'mimic the lm() function in R, output accordingly statistics given the data and formula being provided
+#'
+#'@param formula
+#'
+#'@param data
+#'
+#'@param drop_NA
+#'
+#'@return a list consisting of point estimates, corresponding standard error, t statistic and p-value
+#'
+#'@examples
+#'fit <- myLm(formula = Murder ~ Rape, data = USArrests)
+#'
+#'
+#'
+#'@export
+#'
+myLm = function(formula, data, drop_NA = TRUE) {
+
+}
